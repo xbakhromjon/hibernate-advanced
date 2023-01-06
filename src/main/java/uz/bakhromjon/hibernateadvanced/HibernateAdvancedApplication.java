2b732e95428e569e1028e47caed2dd015eb5c53c
Bakhromjon <xbakhromjon@gmail.com>
2023-01-06 06:41:23 +0500

    init

---
+++ src/main/java/uz/bakhromjon/hibernateadvanced/HibernateAdvancedApplication.java
@@ -0,0 +1,13 @@
+package uz.bakhromjon.hibernateadvanced;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateAdvancedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HibernateAdvancedApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/bakhromjon/hibernateadvanced/HibernateAdvancedApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.bakhromjon.hibernateadvanced;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernateAdvancedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
