50f31c104bfbd548a9639d1bbf8f56a9146b8f01
Bakhromjon <xbakhromjon@gmail.com>
2023-02-18 23:43:55 +0500

    How to map a PostgreSQL ARRAY to a Java List with JPA and Hibernate

--- json/src/main/java/uz/bakhromjon/json/JsonApplication.java
+++ json/src/main/java/uz/bakhromjon/json/JsonApplication.java
@@ -1,6 +1,5 @@
 package uz.bakhromjon.json;
 
-import org.hibernate.annotations.Type;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
