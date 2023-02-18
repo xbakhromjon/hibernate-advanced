69745d46fec91c1bb00dabfa78135c547eeb6ada
Bakhromjon <xbakhromjon@gmail.com>
2023-02-19 00:09:35 +0500

    .

--- json/src/main/java/uz/bakhromjon/json/JsonApplication.java
+++ json/src/main/java/uz/bakhromjon/json/JsonApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JsonApplication {
 
 
+
     public static void main(String[] args) {
         SpringApplication.run(JsonApplication.class, args);
     }
